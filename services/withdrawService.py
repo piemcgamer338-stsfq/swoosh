a5317c64071639935a8a06a9b33a12cb7241de02
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-26 11:42:16 +0530

    Update withdrawService.py

--- services/withdrawService.py
+++ services/withdrawService.py
@@ -1,15 +1,32 @@
 from utils.constants import MIN_WITHDRAW_USD
 
 
-async def request_withdraw(
+async def can_withdraw(user):
+
+    if user.total_deposit <= 0:
+        return False, (
+            "You must deposit at least "
+            "$0.10 before withdrawing."
+        )
+
+    if user.wagered < user.total_deposit * 2:
+        return False, (
+            "You must wager 2x "
+            "your deposits first."
+        )
+
+    return True, None
+
+
+async def create_request(
     user,
     amount,
     address
 ):
 
     return {
-        "approved": False,
+        "user": user.discord_id,
         "amount": amount,
         "address": address,
-        "status": "Waiting For Owner Approval"
+        "status": "Pending"
     }
