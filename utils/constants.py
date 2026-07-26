8334fa4afbd92409a68c46619cfb02c703c143ab
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-26 11:21:19 +0530

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,71 @@
+# Currency
+
+POINT_VALUE = 0.005
+
+
+# Deposit / Withdraw
+
+MIN_DEPOSIT_USD = 0.10
+
+MIN_WITHDRAW_USD = 1.00
+
+WITHDRAW_WAGER_REQUIREMENT = 2
+
+
+
+# Rewards
+
+DAILY_REWARD = 2
+
+WEEKLY_RATE = 0.50
+
+RAKEBACK_RATE = 1.00
+
+
+
+# House
+
+HOUSE_START_BALANCE = 80
+
+
+
+# Games
+
+MAX_BET = 1000000
+
+
+BLACKJACK_RTP = 0.95
+
+COINFLIP_RTP = 0.95
+
+
+
+# Cooldowns (seconds)
+
+DAILY_COOLDOWN = 86400
+
+WEEKLY_COOLDOWN = 604800
+
+
+
+# Rank requirements
+
+RANK_REQUIREMENTS = {
+
+    "Bronze": 0,
+
+    "Silver": 100,
+
+    "Gold": 500,
+
+    "Platinum": 1000,
+
+    "Diamond": 2500,
+
+    "Master": 5000,
+
+    "Legend": 10000,
+
+    "Champion": 25000
+
+}
