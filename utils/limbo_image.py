2aaf85404b5a77db58b0c5aa2e845281bec06135
piemcgamer338-stsfq <piemcgamer338@gmail.com>
2026-07-26 14:50:10 +0530

    Update limbo_image.py

--- utils/limbo_image.py
+++ utils/limbo_image.py
@@ -27,55 +27,6 @@ def get_font(size):
 
 
 
-def fit_font(
-    draw,
-    text,
-    max_width,
-    start_size
-):
-
-    size = start_size
-
-    while size > 10:
-
-        font = get_font(size)
-
-        box = draw.textbbox(
-            (0,0),
-            text,
-            font=font
-        )
-
-        width = box[2] - box[0]
-
-        if width <= max_width:
-            return font
-
-        size -= 5
-
-    return get_font(10)
-
-
-
-def center_position(
-    draw,
-    text,
-    font,
-    width
-):
-
-    box = draw.textbbox(
-        (0,0),
-        text,
-        font=font
-    )
-
-    text_width = box[2] - box[0]
-
-    return (width-text_width)//2
-
-
-
 def create_limbo_image(
     multiplier,
     won
@@ -126,71 +77,65 @@ def create_limbo_image(
 
 
 
-# EXTREME BIG TEXT
+    # BIG CASINO TEXT
 
-multiplier_font = get_font(450)
-result_font = get_font(180)
-
-
-multiplier_box = draw.textbbox(
-    (0, 0),
-    multiplier_text,
-    font=multiplier_font
-)
-
-multiplier_width = multiplier_box[2] - multiplier_box[0]
+    multiplier_font = get_font(
+        450
+    )
 
 
-draw.text(
-    (
-        (width - multiplier_width) // 2,
-        height * 0.18
-    ),
-    multiplier_text,
-    font=multiplier_font,
-    fill=multiplier_color,
-    stroke_width=12,
-    stroke_fill=(0,0,0)
-)
+    result_font = get_font(
+        180
+    )
 
 
 
-result_box = draw.textbbox(
-    (0, 0),
-    result_text,
-    font=result_font
-)
+    # MULTIPLIER TEXT
 
-result_width = result_box[2] - result_box[0]
-
-
-draw.text(
-    (
-        (width-result_width)//2,
-        height * 0.70
-    ),
-    result_text,
-    font=result_font,
-    fill=(255,255,255),
-    stroke_width=8,
-    stroke_fill=(0,0,0)
-)
+    box = draw.textbbox(
+        (0,0),
+        multiplier_text,
+        font=multiplier_font
+    )
 
 
-    # win lose
-
-    x = center_position(
-        draw,
-        result_text,
-        result_font,
-        width
+    multiplier_width = (
+        box[2] - box[0]
     )
 
 
     draw.text(
         (
-            x,
-            height * 0.70
+            (width - multiplier_width)//2,
+            height * 0.15
+        ),
+        multiplier_text,
+        font=multiplier_font,
+        fill=multiplier_color,
+        stroke_width=12,
+        stroke_fill=(0,0,0)
+    )
+
+
+
+    # WIN / LOSE TEXT
+
+    box = draw.textbbox(
+        (0,0),
+        result_text,
+        font=result_font
+    )
+
+
+    result_width = (
+        box[2] - box[0]
+    )
+
+
+    draw.text(
+        (
+            (width-result_width)//2,
+            height * 0.72
         ),
         result_text,
         font=result_font,
